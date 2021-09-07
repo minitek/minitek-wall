9042f54eba9a5e20b262b33f44c66fd14572d098
Yannis Maragos <ioannismaragos@gmail.com>
2021-09-07 17:13:48 +0300

    Fixed bug with duplication of update message

--- admin/tmpl/dashboard/update.php
+++ admin/tmpl/dashboard/update.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
-* @title				Minitek Wall
-* @copyright   	Copyright (C) 2011-2021 Minitek, All rights reserved.
-* @license   		GNU General Public License version 3 or later.
-* @author url   https://www.minitek.gr/
-* @developers   Minitek.gr
-*/
+ * @title				Minitek Wall
+ * @copyright   	Copyright (C) 2011-2021 Minitek, All rights reserved.
+ * @license   		GNU General Public License version 3 or later.
+ * @author url   https://www.minitek.gr/
+ * @developers   Minitek.gr
+ */
 
 defined('_JEXEC') or die;
 
@@ -19,36 +20,36 @@ $latestVersion = MinitekWallHelper::latestVersion();
 $type = Factory::getApplication()->input->get('type', 'auto');
 
 if ($latestVersion && version_compare($latestVersion, $localVersion, '>')) { ?>
-<div class="alert alert-success text-center mt-0" id="update-box">
-  <div class="update-info">
-    <div>
-      <span><?php echo Text::_('COM_MINITEKWALL_DASHBOARD_A_NEW_VERSION_IS_AVAILABLE'); ?></span>
-      <span class="badge bg-success"><?php echo $latestVersion; ?></span> <span class="badge bg-success">Free</span>
-    </div>
-    <div class="mt-2">
-      <a class="button-success btn btn-sm btn-success" href="<?php echo Route::_('index.php?option=com_installer&view=update'); ?>">
-        <span class="icon-refresh" aria-hidden="true"></span>
-        <?php echo Text::_('COM_MINITEKWALL_DASHBOARD_UPDATE_NOW'); ?>
-      </a>
-    </div>
-  </div>
-</div>
-<?php } else if ($type == 'check') { ?>
-  <?php if ($latestVersion) { ?>
-  <div class="alert alert-success text-center mt-0" id="update-box">
+  <div class="alert alert-success text-center mt-0" class="update-box">
     <div class="update-info">
       <div>
-        <?php if ($latestVersion == $localVersion) { ?>
-          <span><?php echo Text::_('COM_MINITEKWALL_DASHBOARD_YOU_HAVE_THE_LATEST_VERSION'); ?></span>
-        <?php } else { ?>
-          <span><?php echo Text::_('COM_MINITEKWALL_DASHBOARD_YOU_HAVE_A_DEVELOPMENT_VERSION'); ?></span>
-        <?php } ?>
+        <span><?php echo Text::_('COM_MINITEKWALL_DASHBOARD_A_NEW_VERSION_IS_AVAILABLE'); ?></span>
         <span class="badge bg-success"><?php echo $latestVersion; ?></span> <span class="badge bg-success">Free</span>
+      </div>
+      <div class="mt-2">
+        <a class="button-success btn btn-sm btn-success" href="<?php echo Route::_('index.php?option=com_installer&view=update'); ?>">
+          <span class="icon-refresh" aria-hidden="true"></span>
+          <?php echo Text::_('COM_MINITEKWALL_DASHBOARD_UPDATE_NOW'); ?>
+        </a>
       </div>
     </div>
   </div>
+<?php } else if ($type == 'check') { ?>
+  <?php if ($latestVersion) { ?>
+    <div class="alert alert-success text-center mt-0" class="update-box">
+      <div class="update-info">
+        <div>
+          <?php if ($latestVersion == $localVersion) { ?>
+            <span><?php echo Text::_('COM_MINITEKWALL_DASHBOARD_YOU_HAVE_THE_LATEST_VERSION'); ?></span>
+          <?php } else { ?>
+            <span><?php echo Text::_('COM_MINITEKWALL_DASHBOARD_YOU_HAVE_A_DEVELOPMENT_VERSION'); ?></span>
+          <?php } ?>
+          <span class="badge bg-success"><?php echo $latestVersion; ?></span> <span class="badge bg-success">Free</span>
+        </div>
+      </div>
+    </div>
   <?php } else { ?>
-    <div class="alert alert-danger text-center mt-0" id="update-box">
+    <div class="alert alert-danger text-center mt-0" class="update-box">
       <div class="update-info">
         <div>
           <span><?php echo Text::_('COM_MINITEKWALL_DASHBOARD_COULD_NOT_FETCH_UPDATE_INFO'); ?></span>
--- media/js/admin-dashboard.js
+++ media/js/admin-dashboard.js
@@ -1,58 +1,61 @@
 (function (document, Joomla) {
-	'use strict';
+  "use strict";
 
-  function checkForUpdate(type)
-	{
+  function checkForUpdate(type) {
     Joomla.request({
-      url: 'index.php?option=com_minitekwall&task=checkForUpdate&type='+type,
-      method: 'GET',
+      url: "index.php?option=com_minitekwall&task=checkForUpdate&type=" + type,
+      method: "GET",
       onSuccess: (response, xhr) => {
-        var dashboard = document.querySelector('.minitek-dashboard');
+        var dashboard = document.querySelector(".minitek-dashboard");
 
-        if (response && dashboard)
-        {
-          var htmlObject = document.createElement('div');
-          htmlObject.id = 'update-box-div';
+        if (response && dashboard) {
+          var update_box = document.querySelectorAll(".update-box-div");
+
+          if (update_box) {
+            update_box.forEach(function (a) {
+              a.remove();
+            });
+          }
+
+          var htmlObject = document.createElement("div");
+          htmlObject.classList.add("update-box-div");
           htmlObject.innerHTML = response;
           dashboard.insertBefore(htmlObject, dashboard.firstChild);
 
-          var check_version = document.querySelector('#check-version');
-          check_version.querySelectorAll('.fas').forEach(function(a)
-          {
-            a.classList.remove('fa-spin');
+          var check_version = document.querySelector("#check-version");
+          check_version.querySelectorAll(".fas").forEach(function (a) {
+            a.classList.remove("fa-spin");
           });
         }
       },
       onError: (xhr) => {
         console.log(xhr);
-      }
+      },
     });
-	}
+  }
 
-  document.addEventListener('DOMContentLoaded', function () 
-	{
-    checkForUpdate('auto');
-    var check_version = document.querySelector('#check-version');
+  document.addEventListener("DOMContentLoaded", function () {
+    checkForUpdate("auto");
+    var check_version = document.querySelector("#check-version");
 
-    if (check_version)
-    {
-      check_version.addEventListener('click', function (e) 
-      {
+    if (check_version) {
+      check_version.addEventListener("click", function (e) {
         e.preventDefault();
 
-        var update_box = document.querySelector('#update-box-div');
+        var update_box = document.querySelectorAll(".update-box-div");
 
-        if (update_box)
-          update_box.remove();
-        
-        check_version.querySelectorAll('.fas').forEach(function(a)
-        {
-          a.classList.add('fa-spin');
+        if (update_box) {
+          update_box.forEach(function (a) {
+            a.remove();
+          });
+        }
+
+        check_version.querySelectorAll(".fas").forEach(function (a) {
+          a.classList.add("fa-spin");
         });
-        
-        checkForUpdate('check');
+
+        checkForUpdate("check");
       });
     }
   });
-
 })(document, Joomla);
