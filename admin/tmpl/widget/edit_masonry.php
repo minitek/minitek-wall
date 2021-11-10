3e1c0d6c854b605b6eb29cb68df1dfac7d8bd5c2
Minitek <info@minitek.gr>
2021-11-10 23:31:31 +0200

    Small changes to widget form

--- admin/tmpl/widget/edit_masonry.php
+++ admin/tmpl/widget/edit_masonry.php
@@ -18,9 +18,9 @@ use Joomla\CMS\Language\Text;
 	<div class="row">
 		<div class="col-12">
 			<fieldset id="fieldset-masonry_layout" class="options-grid-form options-grid-form-full">
-				<div>
-				<?php echo $this->masonryform->renderFieldset('masonry_layout'); ?>
-				</div>
+				<div><?php 
+					echo $this->masonryform->renderFieldset('masonry_layout'); 
+				?></div>
 			</fieldset>
 		</div>
 	</div>
@@ -32,9 +32,9 @@ use Joomla\CMS\Language\Text;
 	<div class="row">
 		<div class="col-12">
 			<fieldset id="fieldset-masonry_image_settings" class="options-grid-form options-grid-form-full">
-				<div>
-				<?php echo $this->masonryform->renderFieldset('masonry_image_settings'); ?>
-				</div>
+				<div><?php 
+					echo $this->masonryform->renderFieldset('masonry_image_settings'); 
+				?></div>
 			</fieldset>
 		</div>
 	</div>
@@ -46,12 +46,14 @@ use Joomla\CMS\Language\Text;
 	<div class="row">
 		<div class="col-12">
 			<fieldset id="fieldset-masonry_detailbox_general" class="options-grid-form options-grid-form-full">
-				<div>
-				<?php echo $this->masonryform->renderFieldset('masonry_detailbox_general'); ?>
-				</div>
+				<div><?php 
+					echo $this->masonryform->renderFieldset('masonry_detailbox_general'); 
+				?></div>
 			</fieldset>
 		</div>
 	</div>
+
+	<br>
 
 	<?php echo HTMLHelper::_('uitab.startTabSet', 'detailBoxTabs', array('active' => 'big')); ?>
 
@@ -60,9 +62,9 @@ use Joomla\CMS\Language\Text;
 		<div class="row">
 			<div class="col-12">
 				<fieldset id="fieldset-masonry_detailbox_big" class="options-grid-form options-grid-form-full">
-					<div>
-					<?php echo $this->masonryform->renderFieldset('masonry_detailbox_big'); ?>
-					</div>
+					<div><?php 
+						echo $this->masonryform->renderFieldset('masonry_detailbox_big'); 
+					?></div>
 				</fieldset>
 			</div>
 		</div>
@@ -74,9 +76,9 @@ use Joomla\CMS\Language\Text;
 		<div class="row">
 			<div class="col-12">
 				<fieldset id="fieldset-masonry_detailbox_landscape" class="options-grid-form options-grid-form-full">
-					<div>
-					<?php echo $this->masonryform->renderFieldset('masonry_detailbox_landscape'); ?>
-					</div>
+					<div><?php 
+						echo $this->masonryform->renderFieldset('masonry_detailbox_landscape'); 
+					?></div>
 				</fieldset>
 			</div>
 		</div>
@@ -88,9 +90,9 @@ use Joomla\CMS\Language\Text;
 		<div class="row">
 			<div class="col-12">
 				<fieldset id="fieldset-masonry_detailbox_portrait" class="options-grid-form options-grid-form-full">
-					<div>
-					<?php echo $this->masonryform->renderFieldset('masonry_detailbox_portrait'); ?>
-					</div>
+					<div><?php 
+						echo $this->masonryform->renderFieldset('masonry_detailbox_portrait'); 
+					?></div>
 				</fieldset>
 			</div>
 		</div>
@@ -102,9 +104,9 @@ use Joomla\CMS\Language\Text;
 		<div class="row">
 			<div class="col-12">
 				<fieldset id="fieldset-masonry_detailbox_small" class="options-grid-form options-grid-form-full">
-					<div>
-					<?php echo $this->masonryform->renderFieldset('masonry_detailbox_small'); ?>
-					</div>
+					<div><?php 
+						echo $this->masonryform->renderFieldset('masonry_detailbox_small'); 
+					?></div>
 				</fieldset>
 			</div>
 		</div>
@@ -116,9 +118,9 @@ use Joomla\CMS\Language\Text;
 		<div class="row">
 			<div class="col-12">
 				<fieldset id="fieldset-masonry_detailbox_column" class="options-grid-form options-grid-form-full">
-					<div>
-					<?php echo $this->masonryform->renderFieldset('masonry_detailbox_column'); ?>
-					</div>
+					<div><?php 
+						echo $this->masonryform->renderFieldset('masonry_detailbox_column'); 
+					?></div>
 				</fieldset>
 			</div>
 		</div>
@@ -134,9 +136,9 @@ use Joomla\CMS\Language\Text;
 	<div class="row">
 		<div class="col-12">
 			<fieldset id="fieldset-masonry_hoverbox" class="options-grid-form options-grid-form-full">
-				<div>
-				<?php echo $this->masonryform->renderFieldset('masonry_hoverbox'); ?>
-				</div>
+				<div><?php 
+					echo $this->masonryform->renderFieldset('masonry_hoverbox'); 
+				?></div>
 			</fieldset>
 		</div>
 	</div>
@@ -148,9 +150,9 @@ use Joomla\CMS\Language\Text;
 	<div class="row">
 		<div class="col-12">
 			<fieldset id="fieldset-masonry_pagination" class="options-grid-form options-grid-form-full">
-				<div>
-				<?php echo $this->masonryform->renderFieldset('masonry_pagination'); ?>
-				</div>
+				<div><?php 
+					echo $this->masonryform->renderFieldset('masonry_pagination'); 
+				?></div>
 			</fieldset>
 		</div>
 	</div>
@@ -162,9 +164,9 @@ use Joomla\CMS\Language\Text;
 	<div class="row">
 		<div class="col-12">
 			<fieldset id="fieldset-masonry_filters" class="options-grid-form options-grid-form-full">
-				<div>
-				<?php echo $this->masonryform->renderFieldset('masonry_filters'); ?>
-				</div>
+				<div><?php 
+					echo $this->masonryform->renderFieldset('masonry_filters'); 
+				?></div>
 			</fieldset>
 		</div>
 	</div>
@@ -176,9 +178,9 @@ use Joomla\CMS\Language\Text;
 	<div class="row">
 		<div class="col-12">
 			<fieldset id="fieldset-masonry_effects" class="options-grid-form options-grid-form-full">
-				<div>
-				<?php echo $this->masonryform->renderFieldset('masonry_effects'); ?>
-				</div>
+				<div><?php 
+					echo $this->masonryform->renderFieldset('masonry_effects'); 
+				?></div>
 			</fieldset>
 		</div>
 	</div>
@@ -190,9 +192,9 @@ use Joomla\CMS\Language\Text;
 	<div class="row">
 		<div class="col-12">
 			<fieldset id="fieldset-masonry_responsive_settings" class="options-grid-form options-grid-form-full">
-				<div>
-				<?php echo $this->masonryform->renderFieldset('masonry_responsive_settings'); ?>
-				</div>
+				<div><?php 
+					echo $this->masonryform->renderFieldset('masonry_responsive_settings'); 
+				?></div>
 			</fieldset>
 		</div>
 	</div>
