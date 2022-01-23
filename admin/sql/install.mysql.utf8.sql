1027fd64cfe8e0e0e317ebb26fd424557dc5409b
Minitek <info@minitek.gr>
2022-01-23 18:39:23 +0200

    Changed checked_out and date columns default value to NULL

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS `#__minitek_wall_widgets` (
  `description` text NOT NULL,
  `masonry_params` text DEFAULT NULL,
  `state` tinyint(1) NOT NULL DEFAULT '0',
- `checked_out` int(10) unsigned NOT NULL DEFAULT '0',
- `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+ `checked_out` int(10) unsigned DEFAULT NULL,
+ `checked_out_time` datetime DEFAULT NULL,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
@@ -24,8 +24,8 @@ CREATE TABLE IF NOT EXISTS `#__minitek_source_groups` (
  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
  `state` tinyint(3) NOT NULL DEFAULT '0',
- `checked_out` int(10) unsigned NOT NULL DEFAULT '0',
- `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+ `checked_out` int(10) unsigned DEFAULT NULL,
+ `checked_out_time` datetime DEFAULT NULL,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
@@ -38,15 +38,15 @@ CREATE TABLE IF NOT EXISTS `#__minitek_source_items` (
  `author` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
  `state` tinyint(3) NOT NULL DEFAULT '0',
  `groupid` int(10) unsigned NOT NULL DEFAULT '0',
- `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+ `created` datetime NOT NULL,
  `created_by` int(10) unsigned NOT NULL DEFAULT '0',
  `created_by_alias` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
- `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+ `modified` datetime NOT NULL,
  `modified_by` int(10) unsigned NOT NULL DEFAULT '0',
- `checked_out` int(10) unsigned NOT NULL DEFAULT '0',
- `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
- `publish_up` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
- `publish_down` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+ `checked_out` int(10) unsigned DEFAULT NULL,
+ `checked_out_time` datetime DEFAULT NULL,
+ `publish_up` datetime DEFAULT NULL,
+ `publish_down` datetime DEFAULT NULL,
  `images` text COLLATE utf8mb4_unicode_ci NOT NULL,
  `urls` text COLLATE utf8mb4_unicode_ci NOT NULL,
  `tags` text COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -62,8 +62,8 @@ CREATE TABLE IF NOT EXISTS `#__minitek_wall_grids` (
  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
  `columns` tinyint(3) unsigned NOT NULL,
  `state` tinyint(3) NOT NULL DEFAULT '0',
- `checked_out` int(10) unsigned NOT NULL DEFAULT '0',
- `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+ `checked_out` int(10) unsigned DEFAULT NULL,
+ `checked_out_time` datetime DEFAULT NULL,
  `elements` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
